bf6793ef2d8be9e8c786011eff952ecc65d91d82
benlow22 <ben2.2low@gmail.com>
2022-10-26 02:37:11 -0700

    Add Database as JS and PostgreSQL

---
+++ database.js
@@ -0,0 +1,6 @@
+
+const listOfTypes = ['Bug',	'Dark',	'Dragon',	'Electric',	'Fairy',	'Fighting',	'Fire',	'Flying',	'Ghost',	'Grass',	'Ground',	'Ice',	'Normal',	'Poison',	'Psychic',	'Rock',	'Steel',	'Water'];
+
+const bugEffectiveness = ['Bug', 1,	1, 1,	1,	1,	0.625,	1.6,	1.6,	1,	0.625,	0.625,	1,	1,	1,1,	1.6,	1,1]
+
+const dataAsAnObject = {'Types':listOfTypes,'Bug':[1, 1, 1,	1, 1, 0.625, 1.6, 1.6, 1,	0.625, 0.625, 1, 1, 1, 1, 1.6, 1,1]};
